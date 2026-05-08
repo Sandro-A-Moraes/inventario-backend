0d91b5f2769d27fc498214bf90252b38770be21f
Sandro-A-Moraes <sandro.corporativo3004@outlook.com>
2026-05-07 23:29:25 -0300

    feat: implement error handling for token verification using AppError

--- src/shared/utils/verifyToken.ts
+++ src/shared/utils/verifyToken.ts
@@ -2,11 +2,20 @@ import jwt from 'jsonwebtoken';
 import type { AccessTokenPayload } from '../../modules/auth/types/access-token-payload';
 import { env } from '../config/env';
 import type { RefreshTokenPayload } from '../../modules/auth/types/refresh-token-payload';
+import { AppError } from '../errors/AppError';
 
 export function verifyAccessToken(token: string): AccessTokenPayload {
-    return jwt.verify(token, env.JWT_SECRET_KEY) as AccessTokenPayload;
+    try {
+        return jwt.verify(token, env.JWT_SECRET_KEY) as AccessTokenPayload;
+    } catch {
+        throw new AppError('Invalid or expired token', 401);
+    }
 }
 
 export function verifyRefreshToken(token: string): RefreshTokenPayload {
-    return jwt.verify(token, env.JWT_REFRESH_SECRET_KEY) as RefreshTokenPayload;
+    try {
+        return jwt.verify(token, env.JWT_REFRESH_SECRET_KEY) as RefreshTokenPayload;
+    } catch {
+        throw new AppError('Invalid or expired token', 401);
+    }
 }
